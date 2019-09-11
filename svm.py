105b92f2a811744bbf039d2bb27106de9559abef
tylerchism <54505656+tylerchism@users.noreply.github.com>
2019-09-10 21:23:55 -0500

    update

--- svm.py
+++ svm.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt 
-import matplotlib import style 
+from matplotlib import style 
 import numpy as np 
 style.use('ggplot')
 
 class support_vector_machine:
-    def__init__(self, visualization=True):
-        sel.visualization = visualization
+    def __init__(self, visualization=True):
+        self.visualization = visualization
         self.colors = {1:'r', -1:'b'}
         if self.visualization:
             self.fig = plt.figure()
@@ -20,7 +20,7 @@ class support_vector_machine:
 
         all_data = []
         for yi in self.data:
-            for featureset in self.dat[yi]
+            for featureset in self.data[yi]:
                 for feature in featureset:
                     all_data.append(feature)
 
@@ -55,7 +55,7 @@ class support_vector_machine:
                         for i in self.data:
                             for xi in self.data[i]:
                                 yi = i
-                                if not yi(np.dot(w_t,xi) + b) >=1:
+                                if not yi*(np.dot(w_t,xi) + b) >= 1:
                                     found_option = False
                         if found_option:
                             opt_dict[np.linalg.norm(w_t)] = [w_t,b]
@@ -73,9 +73,53 @@ class support_vector_machine:
 
     
     def predict(self, features):
-        clssification = np.sign(np.dot(np.array(features), self.w)+self.b) # sign(x.w+b)
+        classification = np.sign(np.dot(np.array(features), self.w)+self.b) # sign(x.w+b)
+        if classification !=0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
         return classification
 
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        # hyperplane = x . w + b
+        # v = x.w+b
+        # positve sv = 1
+        # negative sv = -1
+        # dec = 0
+        def hyperplane(x,w,b,v):
+            return (-w[0] * x - b + v) / w[1]
+        
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[psv1,psv2])
+
+        # (w.x+b) = -1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[nsv1,nsv2])
+
+        # (w.x+b) = 0
+        # support vector hyperplane
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min,hyp_x_max],[db1,db2])
+
+        plt.show()
 
 
-data_dict = {-1:np.array([[1,7],[2,8],[3,8]], 1:np.array([[5,1],[6,-1],[7,3]]))}
+
+data_dict = {-1:np.array([[1,7],[2,8],[3,8]]), 1:np.array([[5,1],[6,-1],[7,3]])}
+
+
+svm = support_vector_machine()
+svm.fit(data=data_dict)
+svm.visualize()
+
+
